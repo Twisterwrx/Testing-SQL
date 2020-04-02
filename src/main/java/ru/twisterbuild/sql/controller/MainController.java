9aafdf4c29f007988235faa6cc96dda95f4c559b
Twisterwrx <kstepa@list.ru>
2020-04-02 20:27:46 +0500

    Ошибка с путями класса

--- src/main/java/ru/twisterbuild/sql/controller/MainController.java
+++ src/main/java/ru/twisterbuild/sql/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ru.twisterbuild.sql.entity.Book;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import ru.twisterbuild.sql.service.BookService;
 
